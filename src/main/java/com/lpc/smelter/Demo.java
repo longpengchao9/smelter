1b61eef35783f57da09ec25f860f509a5c81b341
longpengchao9 <lpc20080916@163.com>
2017-07-31 11:28:42 +0800

    Demo

---
+++ src/main/java/com/lpc/smelter/Demo.java
@@ -0,0 +1,14 @@
+package com.lpc.smelter;
+
+/**
+ * Demo
+ *
+ * @author longpengchao
+ * @email longpengchao@58ganji.com
+ * @create 2017-07-31 11:26
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
